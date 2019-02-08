52b51bba9cb820d88521538402529965241c7f53
Shailen <shailen@gettruck.co.za>
2019-02-08 13:46:04 +0200

    If no url is passed to /api, you will get an error message

--- functions/index.js
+++ functions/index.js
@@ -6,8 +6,14 @@ const getHTML = require("./helpers/get-html");
 module.exports.api = functions.runWith({ memory: "2GB", timeoutSeconds: 60 }).https.onRequest(function(req,res) {
   return cors()(req,res,async function() {
     const url = getURL(req);
-    const html = await getHTML(url);
 
-    return res.send(html);
+    if(url) {
+      const html = await getHTML(url);
+      return res.send(html);
+    } else {
+      return res.json({
+        error: "You need to pass a query of 'url' to /api. Ex. /api?url=https://github.com"
+      });
+    }
   });
 });
