adabae4bee7967842cf0aebd94025b36161258d8
Shailen Naidoo <shailen@gettruck.co.za>
2019-06-17 16:35:52 +0200

    A lot of changes

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import { parser } from './parser';
 const app = express();
 
 app.use(json());
+app.use('/', express.static(`${__dirname}docs/.vuepress/dist`.replace(/(lib|src)/, '')));
+app.get('/', (req: express.Request, res: express.Response): void => {
+  res.sendFile(`${__dirname}docs/.vuepress/dist/index.html`.replace(/(lib|src)/, ''));
+});
 
 app.get('/api', async (req: express.Request, res: express.Response): Promise<void|boolean> => {
   if (!req.query.url) {
