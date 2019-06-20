ac93bbd0ee992e0a2fff2802508bfc195fc290f3
Shailen <shailen@gettruck.co.za>
2019-06-20 18:56:38 +0100

    Moved TS code to root

--- index.ts
+++ index.ts
@@ -4,12 +4,12 @@ import { api } from './routes/api';
 const app = express();
 
 app.use(json());
-app.use('/', express.static(`${__dirname}docs/.vuepress/dist`.replace(/(lib|src)/, '')));
+app.use('/', express.static(`${__dirname}/docs/.vuepress/dist`));
 
 app.use('/api', api);
 
 app.get('/', (req: express.Request, res: express.Response): void => {
-  res.sendFile(`${__dirname}docs/.vuepress/dist/index.html`.replace(/(lib|src)/, ''));
+  res.sendFile(`${__dirname}/docs/.vuepress/dist/index.html`);
 });
 
 app.listen(8080);
