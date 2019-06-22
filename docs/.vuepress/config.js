83a2d9c5af5471c4f00a602787f9d16c89480c5c
Shailen <shailen@gettruck.co.za>
2019-06-22 11:55:24 +0200

    Changed docs font

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,10 +2,12 @@ module.exports = {
   title: 'WebData',
   head: [
     ["link",{ rel: "icon", href: "/logo.png" }],
+    ['link',{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Palanquin&display=swap' }],
     ["link",{ rel: "stylesheet", href: "https://use.fontawesome.com/releases/v5.6.3/css/all.css", integrity: "sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/", crossorigin: "anonymous"}]
   ],
   description: "WebData is a free online REST API that you can use when you want to get any websites meta tag information relating to the Open Graph Protocol",
   themeConfig: {
+    logo: 'logo.png',
     search: false,
     sidebarDepth: 2,
     sidebar: [
